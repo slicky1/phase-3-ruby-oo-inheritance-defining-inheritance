885bd050e02fe67dc03d42496d52ee637a07a335
slicky1 <okunonke@gmail.com>
2021-07-19 01:32:02 -0400

    done

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,22 @@
+require 'pry'
+
 class Vehicle
 
+  attr_accessor :wheel_size, :wheel_number
+
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+
+  def go
+    "vrrrrrrrooom!"
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end
+
 end
+
+# binding.pry
